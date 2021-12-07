547312a2ee32745b400bda53b6abc5c03159038c
Yosha Kunnummal <yoshakunnummal@rocketmortgage.com>
2021-12-07 15:04:20 -0500

    Added all the files

---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using DapperConnection.Models;
+
+namespace DapperConnection.Controllers
+{
+    //To make a controller subclass controller
+    //.net expects your controller name to have the word controller at the end
+    public class RestaurantController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            using(var connect= new MySqlConnection(Secret.Connection))
+            {
+                //so using dapper, we are able to put sql queries into a string
+                //and pass them along to our DB
+                //All Mysql syntax is useable here
+                string sql = "select * from dishes";
+
+                //open our connection to the DB
+                connect.Open();
+
+                //we need to go and pass the sql string to the database
+                //Expecting to return a single model or a list of models, or nothing,
+                //depending on the query
+                List<Dish> dishes= connect.Query<Dish>(sql).ToList();
+
+                //close our connection to our db, if we don't then the databse may not let another
+                //Method use it.
+                connect.Close();
+                Dish d = dishes[0];
+                return View(d);
+            }
+            
+            //return View();
+        }
+    }
+}
---
+++ Models/Dish.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperConnection.Models
+{
+    public class Dish
+    {
+        //This tells C# that Id is our primary keyin our SQL table
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(20)]
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public bool IsHealthy { get; set; }
+        public bool IsVegetarian { get; set; }
+
+        //Range works only on number data types and allows only numbers inclusive between min/max
+        [Range(1,10)]
+        public int SpiceLevel { get; set; }
+    }
+}
